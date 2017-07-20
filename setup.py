82eea82a912eadbabe998349f4c2261ad641441a
Yuanfei Zhu <hi@yuanfeiz.cn>
2017-07-20 21:54:08 +0800

    bump

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import find_packages
 setup(
   name='bitcoin_deposit_worker',
   packages=find_packages(), # this must be the same as the name above
-  version='0.3',
+  version='0.4',
   description='A random test lib',
-  author='Yuanfei ZHu',
+  author='Yuanfei Zuu',
   author_email='hi@yuanfeiz.cn',
   url='https://github.com/yuanfeiz/bitcoin_deposit_worker', # use the URL to the github repo
   download_url='https://github.com/yuanfeiz/bitcoin_deposit_worker/archive/master.zip', # I'll explain this in a second
